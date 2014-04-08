5d257a5ec17276a109d968d592d63dfd48971a5d
Ilya S <public.ilya.s@gmail.com>
2014-04-08 22:15:01 +0700

    add test

--- spec/Ilya/Presenter/BasePresenterSpec.php
+++ spec/Ilya/Presenter/BasePresenterSpec.php
@@ -5,14 +5,40 @@ use Prophecy\Argument;
 
 class BasePresenterSpec extends ObjectBehavior {
 
-    function let()
+    function let(Mock $mock)
     {
-        $this->beConstructedWith(new Mock);
+        $this->beConstructedWith($mock);
     }
 
     function it_is_initializable()
     {
         $this->shouldHaveType('Ilya\Presenter\BasePresenter');
+    }
+
+    function it_redirects_method_calls_to_the_wrapped_object(Mock $mock)
+    {
+        $mock->foo()->shouldBeCalled();
+
+        $this->getWrappedObject()->foo();
+    }
+
+    function it_can_read_a_property_from_the_entity()
+    {
+        // don't see a better way to test it
+        // at least, now
+
+        if ($this->getWrappedObject()->bar != 'baz')
+        {
+            throw new \LogicException();
+        }
+    }
+
+    function it_allows_you_to_override_a_property_by_declaring_a_method()
+    {
+        if ($this->getWrappedObject()->wow != 'amaze')
+        {
+            throw new \LogicException();
+        }
     }
 
 }
