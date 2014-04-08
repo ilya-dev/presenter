7ddfa1e5940ff6704e5335c57a2c75c8cd22d488
Ilya S <public.ilya.s@gmail.com>
2014-04-08 18:31:15 +0700

    initial commit

---
+++ src/Ilya/Presenter/PresenterServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Ilya\Presenter;
+
+use Illuminate\Support\ServiceProvider;
+
+class PresenterServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
